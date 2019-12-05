7ade8fd7be89d084d2103219bf571e48f5349a7e
jiicoding <jiinliin@gmail.com>
2019-12-04 22:46:05 -0800

    1 file modified

--- Slider.js
+++ Slider.js
@@ -1,17 +1,41 @@
 import React,{useState} from 'react';
 import InputRange from 'react-input-range'
-export default function Slider() {
-    const [value, setValue] = useState(4);
 
-  return (<div> slider<br/>
-          <InputRange
+function SliderOne() {
+  const  [value, setValue] = useState(4);
+
+  return (<InputRange
           draggableTrack
           maxValue={20}
           minValue={0}
           onChange={v => setValue(v) }
           onChangeComplete={value => console.log(value)}
-          value={value} />
+          value={value} />)
 
-  
+
+}
+
+function SliderTwo() {
+  const  [value, setValue] = useState({min:3,max:38});
+
+  return (<InputRange
+          draggableTrack
+          maxValue={100}
+          minValue={0}
+          onChange={v => setValue(v) }
+          onChangeComplete={value => console.log(value)}
+          value={value} />)
+
+
+}
+
+export default function Slider() {
+    const [value, setValue] = useState(4);
+
+  return (<div> slider<br/>
+         <SliderOne/>
+         <SliderTwo/>
   </div>)
 }
+
+
